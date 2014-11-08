e29f4e1b80f84c9171df9004ba71a4ffb19140dd
kriyss <kriyssou@gmail.com>
2014-11-08 23:23:51 +0100

    change algo's implementation

---
+++ src/com/tamina/cow4/gen/Cell.hx
@@ -0,0 +1,23 @@
+package com.tamina.cow4.gen;
+
+import createjs.easeljs.Point;
+import org.tamina.utils.UID;
+
+class Cell {
+
+    public var visited  : Bool;
+    public var id       : Float;
+
+    public var nexts      :Array<Cell>;
+    public var previous   :Array<Cell>;
+
+    public var position :Point;
+
+    public function new(position:Point) {
+        this.id         = UID.getUID();
+        this.visited    = false;
+        this.nexts      = new Array<Cell>();
+        this.previous   = new Array<Cell>();
+        this.position   = position;
+    }
+}
